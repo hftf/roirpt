f836379e34dbab4dfc69229e36d3000fdf69036d
Ophir Lifshitz <hangfromthefloor@gmail.com>
2023-10-03 07:05:55 -0400

    Refactor

--- jeff-phrasing/my_phrasing.py
+++ jeff-phrasing/my_phrasing.py
@@ -2,21 +2,16 @@ import noun_data, verb_data, jeff_phrasing
 import re
 
 STROKE_PARTS = re.compile(r'''\#?
-	(?P<question>    \^?)
-	(?P<contraction> \+?)
-	(?P<starter>     S?T?K?P?W?H?R?)
-	(?P<modal>       A?O?)-?
-	(?P<negation>    \*?)
-	(?P<aspect>      E?U?)
-	(?P<ender>       F?R?P?B?L?G?T?S?D?Z?)''', # note: D is tense
+	(?P<question> \^?)
+	(?P<contract> \+?)
+	(?P<starter>  S?T?K?P?W?H?R?)
+	(?P<modal>    A?O?)-?
+	(?P<negation> \*?)
+	(?P<aspect>   E?U?)
+	(?P<ender>    F?R?P?B?L?G?T?S?D?Z?)''', # note: D is tense
 	re.X)
 
-MODALS = {
-	'':   None,
-	'A':  'can',
-	'AO': 'will',
-	'O':  'shall',
-}
+MODALS = {'': None, 'A': 'can', 'AO': 'will', 'O': 'shall'}
 NEGATIVE_CONTRACTION_BASES = {'can': 'ca', 'will': 'wo', 'shall': 'sha'}
 CONTRACTIONS = {'am': "'m", 'are': "'re", 'is': "'s", 'has': "'s",
 	'will': "'ll", 'would': "'d", 'had': "'d", 'have': "'ve"}
@@ -25,7 +20,7 @@ jeff_phrasing.SIMPLE_PRONOUNS[''] = ('', None, None)
 
 def stroke_to_obj(stroke):
 	stroke = STROKE_PARTS.match(stroke)
-	question, contraction, starter, modal, negation, aspect, ender = stroke.groups()
+	question, contract, starter, modal, negation, aspect, ender = stroke.groups()
 
 	data = {}
 	# SIMPLE STARTER
@@ -39,12 +34,12 @@ def stroke_to_obj(stroke):
 			subject = jeff_phrasing.SIMPLE_PRONOUNS[simple_pronoun][0]
 			data.update(noun_data.noun_data[subject])
 
-		data['have']        = False
-		data['be']          = False
-		data['modal']       = None
-		data['question']    = False
-		data['negation']    = False
-		data['contraction'] = False
+		data['have']     = False
+		data['be']       = False
+		data['modal']    = None
+		data['question'] = False
+		data['negation'] = False
+		data['contract'] = False
 
 	# NORMAL STARTER
 	elif starter in noun_data.STARTERS:
@@ -52,13 +47,13 @@ def stroke_to_obj(stroke):
 		data.update(noun_data.noun_data[subject])
 
 		data['cosubordinator'] = None
-		data['have']        = 'E' in aspect
-		data['be']          = 'U' in aspect
-		data['modal']       = MODALS[modal]
-		data['question']    = question == '^'
-		data['negation']    = negation == '*'
-		data['contraction'] = contraction == '+'
-		# data['tense']       = 'D' in ender
+		data['have']     = 'E' in aspect
+		data['be']       = 'U' in aspect
+		data['modal']    = MODALS[modal]
+		data['question'] = question == '^'
+		data['negation'] = negation == '*'
+		data['contract'] = contract == '+'
+		# data['tense']    = 'D' in ender
 
 	else:
 		raise KeyError(f'Starter {starter} not found')
@@ -74,15 +69,9 @@ def stroke_to_obj(stroke):
 	return data
 
 
-def pick_lookup(vs, ks, d):
-	subset = dict((k, vs[k]) for k in ks)
-	for k, v in d.items():
-		if v == subset:
-			return k
-
 def obj_to_phrase(obj):
-	subject, person, number, tense, modal, have, be, verb, question, negation, contraction, cosubordinator, extra_word = (obj[k] for k in
-	'subject, person, number, tense, modal, have, be, verb, question, negation, contraction, cosubordinator, extra_word'.split(', '))
+	subject, person, number, tense, modal, have, be, verb, question, negation, contract, cosubordinator, extra_word = (obj[k] for k in
+	'subject, person, number, tense, modal, have, be, verb, question, negation, contract, cosubordinator, extra_word'.split(', '))
 
 	phrase = []
 	finite = not (subject == '' and question)
@@ -114,7 +103,7 @@ def obj_to_phrase(obj):
 		phrase[i] = forms[select]
 
 	if negation:
-		if contraction and finite and phrase[0] != 'am':
+		if contract and finite and phrase[0] != 'am':
 			if phrase[0] in NEGATIVE_CONTRACTION_BASES:
 				phrase[0] = NEGATIVE_CONTRACTION_BASES[phrase[0]]
 			phrase[0] += "n't"
@@ -125,11 +114,9 @@ def obj_to_phrase(obj):
 
 	# inversion
 	if subject:
+		if contract and not question and phrase[0] in CONTRACTIONS:
+			subject += CONTRACTIONS[phrase.pop(0)]
 		phrase.insert(question, subject)
-
-		if contraction and not question:
-			if phrase[1] in CONTRACTIONS:
-				phrase[0] = phrase[0] + CONTRACTIONS[phrase.pop(1)]
 
 	if cosubordinator:
 		phrase.insert(0, cosubordinator)
@@ -170,7 +157,7 @@ test_obj_1 = {
 	'extra_word': 'to',
 
 	# ’  True, False
-	'contracted': False,
+	'contract': False,
 
 	# A  None, just, really, even, still, always, never
 	'adverb': None,
